e4bdeec266b110fc695c17d3d61dae5d1befaf9d
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-05 13:43:00 +0100

    Step 1 initial checkin

---
+++ src/Survivor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ZombieSurvivor.Core
+{
+    public class Survivor
+    {
+        public string Name { get; }
+        public int Wounds { get; private set; } = 0;
+        public bool IsAlive => Wounds < MaxWounds;
+
+        public int ActionsRemaining { get; set; } = ActionsPerTurn;
+        public const int ActionsPerTurn = 3;
+        public const int MaxWounds = 2;
+
+        public Survivor(string name)
+        {
+            Name = name;
+        }
+
+        public void SustainInjury(int numberOfWounds) => Wounds = Wounds + numberOfWounds <= MaxWounds
+                ? Wounds + numberOfWounds
+                : MaxWounds;
+    }
+}
