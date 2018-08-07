194e56cea3ba76a5a76fe9fec40ff5394d6f68b6
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-07 18:42:14 +0100

    Added Game class and coded Step 3 fetures

---
+++ UnitTesting/GameShould.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using ZombieSurvivor.Core;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class GameShould
+    {
+        [TestMethod]
+        public void StartOutWithNoSurvivors()
+        {
+            var game = new Game();
+
+            Assert.AreEqual(0, game.Survivors.Count());
+        }
+
+        [TestMethod]
+        public void AddNewSurvivor_GivenNoCurrentSurvivors()
+        {
+            var game = new Game();
+            var bill = new Survivor("Bill");
+            var successfullyAddedSurvivor = game.AddSurvivorToGame(bill);
+
+            Assert.IsTrue(successfullyAddedSurvivor);
+            Assert.AreEqual(1, game.Survivors.Count());
+        }
+
+        [TestMethod]
+        public void NotAddNewSurvivor_GivenSurvivorWithSameNameAlreadyInGame()
+        {
+            var game = new Game();
+            var bill = new Survivor("Bill");
+            game.AddSurvivorToGame(bill);
+
+            var differentBill = new Survivor("Bill");
+            var successfullyAddedSurvivor = game.AddSurvivorToGame(differentBill);
+
+            Assert.IsFalse(successfullyAddedSurvivor);
+        }
+    }
+}
---
+++ src/Game.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ZombieSurvivor.Core
+{
+    public class Game
+    {
+        public IList<Survivor> Survivors { get; set; }
+        public bool IsEndOfGame => (Survivors.Count > 0 && !Survivors.Any(s => s.IsAlive));
+
+        public Game()
+        {
+            Survivors = new List<Survivor>();
+        }
+
+        public bool AddSurvivorToGame(Survivor newSurvivor)
+        {
+            var retVal = false;
+            var newSurvivorName = newSurvivor.Name;
+
+            if (!Survivors.Any(s => s.Name.Equals(newSurvivorName)))
+            {
+                Survivors.Add(newSurvivor);
+                retVal = true;
+            }
+
+            return retVal;
+        }
+    }
+}
