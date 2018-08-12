69986bc358c8cb0f3f5e10b5352f82fbe593efdd
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-12 11:52:05 +0100

    Added DebuggerDisplay attribute to GameEvent class

--- src/GameEvent.cs
+++ src/GameEvent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ZombieSurvivor.Core
 {
+    [DebuggerDisplay("{EventDetail}")]
     public class GameEvent
     {
         public DateTime EventDateTime { get; }
